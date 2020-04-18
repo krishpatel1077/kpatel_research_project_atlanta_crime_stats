d5e774f89932f20a7d99e73bd3e0d3242b6f16c0
Krish Patel <2021kpatel@gmail.com>
2020-04-18 09:31:57 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,2 @@
 # Atlanta-Crime-Statistics-2019-Miller-Porcelli-Research-Project
 Data Sheet / Analysis for "Injury Timeout: American Cities Hosting Super Bowls are Endangering Their Citizens Lives" - Research paper by Krish Patel
-
-FOR GRAPH LEGIBILITY:
-  Singular Offense Graphs:
-    YTD 2019 Cases - Blue
-    YTD 2018 Cases - Orange
-  Multi Offense Graphs:
-    YTD 2019 Murder - Blue
-    YTD 2018 Murder - Orange
-    YTD 2019 Rape - Green
-    YTD 2018 Rape - Red
-    YTD 2019 Robbery - Purple
-    YTD 2018 Robbery - Magenta
-    YTD 2019 Aggravated Assault - Pink
-    YTD 2018 Aggravated Assault - Grey
--- graphreader.py
+++ graphreader.py
@@ -1,45 +1,24 @@
 #Krish Patel, 2020
-import matplotlib.pyplot as plt
-import csv
+from matplotlib import pyplot as plt
+import pandas as pd
+dataframe = pd.read_csv("DataSheetAtlanta.csv")
 
-x = []
-murder_new = []
-murder_old = []
-rape_new = []
-rape_old = []
-robbery_new = []
-robbery_old = []
-agg_ass_new = []
-agg_ass_old = []
+time = dataframe.WeeksoftheYear
+murder_new = dataframe.YTD2019Murder
+murder_old = dataframe.YTD2018Murder
+rape_new = dataframe.YTD2019Rape
+rape_old = dataframe.YTD2018Rape
+robbery_new = dataframe.YTD2019Robbery
+robbery_old = dataframe.YTD2018Robbery
+aggass_new = dataframe.YTD2019AggAssault
+aggass_old = dataframe.YTD2018AggAssault
 
-with open('DataSheetAtlanta.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append((row[0]))
-        murder_new.append((row[1]))
-        rape_new.append((row[2]))
-        robbery_new.append((row[3]))
-        agg_ass_new.append((row[4]))
-        murder_old.append((row[5]))
-        rape_old.append((row[6]))
-        robbery_old.append((row[7]))
-        agg_ass_new.append((row[8]))
-
-
-plt.plot(x,murder_new, label='2019 Murder')
-plt.plot(x,murder_old, label='2018 Murder')
-
-plt.plot(x,rape_new, label='2019 Rape')
-plt.plot(x,rape_old, label='2018 Rape')
-
-plt.plot(x,robbery_new, label='2019 Robbery')
-plt.plot(x,robbery_old, label='2018 Robbery')
-
-plt.plot(x,agg_ass_new, label='2019 Aggravated Assault')
-plt.plot(x,agg_ass_old, label='2018 Aggravated Assault')
-
-plt.xlabel('Time (weeks)')
-plt.ylabel('Cases (#)')
-plt.title('Major Crime Rates - Atlanta 2018/2019')
-plt.legend()
+plt.scatter(time, murder_new)
+plt.scatter(time, murder_old)
+plt.scatter(time, rape_new)
+plt.scatter(time, rape_old)
+plt.scatter(time, robbery_new)
+plt.scatter(time, robbery_old)
+plt.scatter(time, aggass_new)
+plt.scatter(time, aggass_old)
 plt.show()
